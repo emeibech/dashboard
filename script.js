cc04887a2a90f24b9392ae382f3e366163def81f
emeibech <emeibech@gmail.com>
2022-09-19 16:20:19 +0800

    Add interaction icons

---
+++ script.js
@@ -0,0 +1,54 @@
+const stars = document.querySelectorAll('.star');
+const shares = document.querySelectorAll('.share');
+const visibilities = document.querySelectorAll('.visibility');
+
+//hover effect for stars in interaction section
+stars.forEach(star => {
+    star.addEventListener('mouseover', starHoverColor);
+});
+
+stars.forEach(star => {
+    star.addEventListener('mouseout', starDefaultColor);
+});
+
+function starDefaultColor(e) {
+    e.target.src="./svg/star.svg";
+}
+
+function starHoverColor(e) {
+    e.target.src="./svg/star-hover.svg";
+}
+
+//hover effect for shares in interaction section
+shares.forEach(share => {
+    share.addEventListener('mouseover', shareHoverColor);
+});
+
+shares.forEach(share => {
+    share.addEventListener('mouseout', shareDefaultColor);
+});
+
+function shareDefaultColor(e) {
+    e.target.src="./svg/share.svg";
+}
+
+function shareHoverColor(e) {
+    e.target.src="./svg/share-hover.svg";
+}
+
+//hover effect for visibilities in interaction section
+visibilities.forEach(visibility => {
+    visibility.addEventListener('mouseover', visibilityHoverColor);
+});
+
+visibilities.forEach(visibility => {
+    visibility.addEventListener('mouseout', visibilityDefaultColor);
+});
+
+function visibilityDefaultColor(e) {
+    e.target.src="./svg/visibility.svg";
+}
+
+function visibilityHoverColor(e) {
+    e.target.src="./svg/visibility-hover.svg";
+}
